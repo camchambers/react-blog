4499e0ec520946d790fc4c71118780575667d324
Cam Chambers <cam_chambers@outlook.com>
2021-09-10 18:43:27 -0400

    Article pages now dynamically render

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-
+import articleContent from './article-content';
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
   return (
     <React.Fragment>
-      <h1>This is the {name} article</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-        est laborum.
-      </p>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
     </React.Fragment>
   );
 }

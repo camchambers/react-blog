aac2b4fed87d458deb975004ccc837e451d0fdd7
Cam Chambers <cam_chambers@outlook.com>
2021-09-11 11:40:17 -0400

    Copleted an article listing page

--- src/pages/ArticlesList.jsx
+++ src/pages/ArticlesList.jsx
@@ -1,17 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
   <React.Fragment>
     <h1>Articles</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
+    {articleContent.map((article, key) => (
+      <Link key={key} to={`/article/${article.name}`}>
+        <h3>{article.title}</h3>
+      </Link>
+    ))}
   </React.Fragment>
 );
 

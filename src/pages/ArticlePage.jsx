19eddeea28f5795060c5885c9e8f30daf6cbe260
Cam Chambers <cam_chambers@outlook.com>
2021-09-12 10:08:27 -0400

    Article page now shows 404 if article page is not found

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from "./NotFoundPage";
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
-  if (!article) return <h1>Article does not exist!</h1>
+  if (!article) return <NotFoundPage />
 
   // Filter article content so that we list other articles except the one 
   // that we are currently viewing.
@@ -18,6 +19,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles</h3>
       <ArticlesList articles={otherArticles} />
     </React.Fragment>
   );

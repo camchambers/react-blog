70b4683aa82e14b757574fbc6abb2b78d73b149a
Cam Chambers <cam_chambers@outlook.com>
2021-09-12 09:02:49 -0400

    Reused the ArticlesList component
    Created a "related articles" component on the articles page
    to demonstrate reusing the ArticlesList component.

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
+
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
-  
+
   if (!article) return <h1>Article does not exist!</h1>
+
+  // Filter article content so that we list other articles except the one 
+  // that we are currently viewing.
+  const otherArticles = articleContent.filter(article => article.name !== name);
 
   return (
     <React.Fragment>
@@ -12,6 +18,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <ArticlesList articles={otherArticles} />
     </React.Fragment>
   );
 }

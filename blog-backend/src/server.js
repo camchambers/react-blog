ee4b41d6bb61a98a5c20a13d2d7cd4031e983742
Cam Chambers <cam_chambers@outlook.com>
2021-09-13 11:50:55 -0400

    Updated dependencies

---
+++ blog-backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/hello", (req, res) => res.send("Hello!"));
+app.post("/hello", (req, res) => res.send(`Hello! ${req.body.name}`));
+
+app.listen(8000, () => console.log("Listening on port 8000"));

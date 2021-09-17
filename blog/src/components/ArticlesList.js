52a3697dfda7ba5190fe312da38aa7977b095693
Cam Chambers <cam_chambers@outlook.com>
2021-09-17 15:54:07 -0400

    Cleanup

--- blog/src/components/ArticlesList.js
+++ blog/src/components/ArticlesList.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ArticlesList = ({ articles }) => (
   <React.Fragment>
     {articles.map((article, key) => (
-      <Link key={key} to={`/article/${article.name}`}>
+      <Link
+        className="article-list-item"
+        key={key}
+        to={`/article/${article.name}`}
+      >
         <h3>{article.title}</h3>
         <p>{article.content[0].substring(0, 150)}...</p>
       </Link>

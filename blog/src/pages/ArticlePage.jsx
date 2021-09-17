be0d4468cc71c92c95725ac9c5fb9a949c770162
Cam Chambers <cam_chambers@outlook.com>
2021-09-16 22:02:32 -0400

    Article upvotes are fetched and process from the DB

--- blog/src/pages/ArticlePage.jsx
+++ blog/src/pages/ArticlePage.jsx
@@ -1,37 +1,42 @@
-import { useState, useEffect } from 'react';
-import 'whatwg-fetch';
-import React from "react";
-import ArticlesList from '../components/ArticlesList';
+import React, { useState, useEffect } from "react";
+import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
-import articleContent from './article-content';
+import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
-  const article = articleContent.find(article => article.name === name);
+  const article = articleContent.find((article) => article.name === name);
 
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
   useEffect(() => {
-    setArticleInfo({ upvotes: 3 })
-  })
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log("testing123");
+      //console.log(body);
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, );
 
-  if (!article) return <NotFoundPage />
+  if (!article) return <NotFoundPage />;
 
-  // Filter article content so that we list other articles except the one 
-  // that we are currently viewing.
-  const otherArticles = articleContent.filter(article => article.name !== name);
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
 
   return (
     <React.Fragment>
       <h1>{article.title}</h1>
-      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
-      <h3>Other Articles</h3>
+      <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </React.Fragment>
   );
-}
+};
 
 export default ArticlePage;
